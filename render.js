80648f03f4425e6d05f76eb2f5ba1874575506e4
henryN98 <henry.nur137@gmail.com>
2023-02-14 17:09:59 +0200

    changed the div id to data-id and append comments to the data-id

--- render.js
+++ render.js
@@ -2,25 +2,31 @@ const Render = function () {
   const renderPosts = function (posts) {
     for (let post of posts) {
       //append post
-      let newPost = `<div class="post" id="${post.id}"><h2 class="post-text">${post.text}</h2></div>`;
-      $("#posts").append(newPost);
-      let divForComments = `<div  id="pc${post.id}"></div>`;
-      $(`#${post.id}`).append(divForComments);
+      let newPost = $(
+        `<div data-id="${post.id}" class="post">${post.text}</div>`
+      );
+      $(`#posts`).append(newPost);
+
+      let divForComments = `<div  data-id="pc${post.id}"></div>`;
+      $(`[data-id=${post.id}]`).append(divForComments);
+
       //append comments
       for (let comment of post.comments) {
         let newComment = `<div class="comments"><span data-commentId="${comment.id}" data-postId="${post.id}" class="delete-comment">x </span>${comment.text}</div>`;
-        $(`#pc${post.id}`).append(newComment);
+        $(`[data-id=pc${post.id}]`).append(newComment);
       }
-      //append input and button for comment
+
+      // //append input and button for comment
       let newInput = `<input data-id="${post.id}" class="input" type="text">`;
       let addComment = `<button data-id="${post.id}" class="btns">Comment</button>`;
       let br = `<br>`;
-      $(`#pc${post.id}`).append(br);
-      $(`#pc${post.id}`).append(newInput);
-      $(`#pc${post.id}`).append(addComment);
+      $(`[data-id=pc${post.id}]`).append(br);
+      $(`[data-id=pc${post.id}]`).append(newInput);
+      $(`[data-id=pc${post.id}]`).append(addComment);
+
       ///append delete button for post
       let deleteButton = `<div><button class="delete" data-id="${post.id}">Delete Post</button></div>`;
-      $(`#pc${post.id}`).append(deleteButton);
+      $(`[data-id=pc${post.id}]`).append(deleteButton);
     }
   };
   return {
